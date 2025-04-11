25d18137152719fde823090e7a8c709ec47c0614
Phanindra <phanindragadireddy2@gmail.com>
2025-04-11 14:40:31 +0530

    content added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import AnotherApi from './AnotherApi';
+import Another from './Another';
 
 function App() {
   return (
     <div className="App">
-     <AnotherApi />
+     <Another />
     </div>
   );
 }
